85c6b79d5a8283130d5b09ce72de124ab4a65db5
omotega <tomoyibo@gmail.com>
2022-11-14 19:18:21 +0100

    added signup function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,3 +4,6 @@ import { IOtp, IUser } from '../utils/interface';
 import Users from '../models/userModel';
 import OTP from '../models/Otp';
 import { otp } from '../utils/generateOtp';
+import { successResponse, errorResponse, handleError } from '../utils/response'
+
+
