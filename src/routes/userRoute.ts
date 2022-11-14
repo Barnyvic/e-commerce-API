59e0aea6ec076cefe547f26cbe990bcd471b4171
omotega <tomoyibo@gmail.com>
2022-11-14 21:42:05 +0100

    added the login route to my route file

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 
 const userRouter = Router()
 
-import { createUser } from '../controllers/userController'
+import { createUser,login } from '../controllers/userController'
 
 userRouter.route('/signup').post(createUser);
+userRouter.route('/login').get(login);
 
 export default userRouter;
