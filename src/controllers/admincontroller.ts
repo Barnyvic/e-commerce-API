a70a24260465d4f28ed35e16236da378ce177961
omotega <tomoyibo@gmail.com>
2022-11-16 21:02:59 +0100

    added createVendor function to the admin controller

---
+++ src/controllers/admincontroller.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from 'express';
+import { generateToken } from '../utils/jwt';
+import { IOtp, IUser } from '../utils/interface';
+// import sendEmail from '../service/mailgun';
+import sendEmail from '../utils/email';
+import otpGenerator from 'otp-generator';
+import Users from '../models/userModel';
+import OTP from '../models/Otp';
+import { successResponse, errorResponse, handleError } from '../utils/response';
+import { hashPassword, comparePassword } from '../utils/hash';
+
+export const createVendor = async (req: Request, res: Response) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+      confirmPassword,
+      role,
+    } = req.body;
+
+    //    making sure all fields are valid
+    if (
+      !email ||
+      !password ||
+      !firstName ||
+      !lastName ||
+      !phone ||
+      !confirmPassword ||
+      !role
+    ) {
+      return errorResponse(res, 400, 'Please Fill empty fields');
+    }
+    //   confirming password
+    if (password !== confirmPassword) {
+      return errorResponse(res, 400, 'PassWord must Match');
+    }
+
+    const emialExist = await Users.findOne({ email });
+    if (emialExist)
+      return errorResponse(res, 409, 'email already in use by another user');
+
+    const phoneExist = await Users.findOne({ phone });
+
+    if (phoneExist)
+      return errorResponse(
+        res,
+        409,
+        'Phone Number already in use by another user'
+      );
+
+    const hash = await hashPassword(password);
+
+    // save user to db
+    const vendor = await Users.create({
+      firstName,
+      lastName,
+      email,
+      phone,
+      password: hash,
+      role,
+    });
+
+    // generate otp
+    const otp = otpGenerator.generate(6, {
+      digits: true,
+      lowerCaseAlphabets: false,
+      upperCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log(otp);
+
+    await OTP.create({ email, token: otp });
+    const subject = 'Vendor created';
+    const message = `hi, thank you for signing up kindly verify your account with this token ${otp}`;
+    console.log(subject);
+    await sendEmail(email, subject, message);
+
+    return successResponse(
+      res,
+      201,
+      'Account created successfully, kindly verify your email and login.',
+      vendor
+    );
+  } catch (error) {
+    handleError(req, error);
+    return errorResponse(res, 500, 'Server error.');
+  }
+};
