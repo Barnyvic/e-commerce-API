e23e728e0c96e50fd78e479757239db397c2e80c
omotega <tomoyibo@gmail.com>
2022-11-22 05:55:23 +0100

    fixed the error in the email utils

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,26 +1,28 @@
-import sgMail from '@sendgrid/mail';
+import sgMail from "@sendgrid/mail";
+import dotenv from 'dotenv'
+dotenv.config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 const msg: any = {
-  from: `ecommerce <${process.env.SENGRID_EMAIL}>`,
-  mail_setting: { sandbox_mode: { enable: false } },
+  from: `e-commerce <${process.env.SENDGRID_EMAIL}>`,
+  mail_settings: { sandbox_mode: { enable: false } }
 };
 
 () => {
-  msg.mail_setting.sandbox_mode.enable = true;
+  msg.mail_settings.sandbox_mode.enable = true;
 };
 
 const sendEmail = async (email: string, subject: string, message: string) => {
   try {
-    (msg.to = email),
-      (msg.subject = subject),
-      (msg.text = message),
-      await sgMail.send(msg);
-    console.log('message sent');
-  } catch (error) {
-    return error;
+    msg.to = email;
+    msg.subject = subject;
+    msg.text = message;
+    await sgMail.send(msg);
+    console.log("message sent...");
+  } catch (err) {
+    return err;
   }
 };
 
-export default sendEmail;
+export default sendEmail
