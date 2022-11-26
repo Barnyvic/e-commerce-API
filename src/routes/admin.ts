0abb77f9b9be9a5b9173d9b04b4dbea790c65698
omotega <tomoyibo@gmail.com>
2022-11-26 20:17:43 +0100

    add activete deactivated user route to the admin route file

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 const adminRouter = Router();
 
-import { createVendor,deactivateUser,updateUserRole } from '../controllers/admincontroller';
+import { createVendor,deactivateUser,updateUserRole,activateDeactivatedUser } from '../controllers/admincontroller';
 import { authguard, verifyAdmin } from '../middlewares/auth';
 
 adminRouter.route('/update/:userId').patch(authguard,verifyAdmin,updateUserRole);
-adminRouter.route('/active/:userId').put(authguard,verifyAdmin,deactivateUser);
+adminRouter.route('/deactivate/:userId').put(authguard,verifyAdmin,deactivateUser);
+adminRouter.route('/activate/:userId').patch(authguard,verifyAdmin,activateDeactivatedUser);
 adminRouter.route('/create').post(authguard, verifyAdmin, createVendor);
+
 
 
 export default adminRouter;
