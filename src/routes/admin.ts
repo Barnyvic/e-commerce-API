9fe1024da6ef14d8a20e462b49cb8c0023594d7f
omotega <tomoyibo@gmail.com>
2022-11-26 20:00:39 +0100

    added update role route to the admin route file

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 
 const adminRouter = Router();
 
-import { createVendor } from '../controllers/admincontroller';
+import { createVendor,deactivateUser,updateUserRole } from '../controllers/admincontroller';
 import { authguard, verifyAdmin } from '../middlewares/auth';
 
+adminRouter.route('/update/:userId').patch(authguard,verifyAdmin,updateUserRole);
+adminRouter.route('/active/:userId').put(authguard,verifyAdmin,deactivateUser);
 adminRouter.route('/create').post(authguard, verifyAdmin, createVendor);
+
 
 export default adminRouter;
